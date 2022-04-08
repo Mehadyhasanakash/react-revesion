508f0d0ba008cde17c5b9bc2619c379c83b8aba4
Mehadyhasanakash <mehadyhasanakash@gmail.com>
2022-04-08 11:45:48 +0600

    frsit-commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Resturant from './Component/Basic/Resturant';
+
+
+function App() {
+  return (
+    <div>
+     <Resturant></Resturant>
+    </div>
+  );
+}
+
+export default App;
