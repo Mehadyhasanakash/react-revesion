5194f8668fd76432efc4ba676bf3f5146d56e61c
Mehadyhasanakash <mehadyhasanakash@gmail.com>
2022-04-08 12:28:10 +0600

    3rd-commit

--- src/Component/Basic/Resturant.js
+++ src/Component/Basic/Resturant.js
@@ -1,31 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./style.css"
-import Menu  from "../MenuApi.json/MenuApi"
+import Menu from './MenuApi'
+import ResturantCard from './ResturantCard';
 
 const Resturant = () => {
+
+    const [menuDatas, setMenuData] = useState(Menu)
     return (
         <>
-           <div className='card-container'>
-               <div className='card'>
-                   <div className='card-body'>
-                       <span className='card-number card-circle subtle'>1</span>
-                       <span className='card-author'> Breakfast</span>
-                       
-                       <h2 className='card-title'> Maggi</h2>
-
-                       <span className='card-description subtle'>
-
-                           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum temporibus odit accusamus fugit eligendi animi aspernatur, consectetur omnis hic eveniet expedita nobis molestias delectus quia commodi? Perferendis quam velit nam dolores provident ipsa, accusantium, excepturi aliquam eius officia perspiciatis tempore.
-                       </span>
-                        <div className='card-read'>Read</div>
-                   </div>
-                   <img src={image} alt="image" className='card-media' />
-
-                   <span className='card-tag'> oder now</span>
-
-               </div>
-
-           </div>
+        <ResturantCard menu={menuDatas}></ResturantCard> 
         </>
     );
 };
---
+++ src/Component/Basic/ResturantCard.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const ResturantCard = ({menu}) => {
+    console.log(menu)
+    return (
+        <>
+         <div className='card-container'>
+               <div className='card'>
+                   <div className='card-body'>
+                       <span className='card-number card-circle subtle'>1</span>
+                       <span className='card-author'> Breakfast</span>
+                       
+                       <h2 className='card-title'> Maggi</h2>
+
+                       <span className='card-description subtle'>
+
+                           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum temporibus odit accusamus fugit eligendi animi aspernatur, consectetur omnis hic eveniet expedita nobis molestias delectus quia commodi? Perferendis quam velit nam dolores provident ipsa, accusantium, excepturi aliquam eius officia perspiciatis tempore.
+                       </span>
+                        <div className='card-read'>Read</div>
+                   </div>
+                   {/* <img src={image} alt="image" className='card-media' /> */}
+
+                   <span className='card-tag'> oder now</span>
+
+               </div>
+
+           </div>
+            
+        </>
+    );
+};
+
+export default ResturantCard;
