9a6096d801fadc57767d2bca5668d4212b512905
Mehadyhasanakash <mehadyhasanakash@gmail.com>
2022-04-08 16:01:52 +0600

    brackfast-item

--- src/Component/Basic/Resturant.js
+++ src/Component/Basic/Resturant.js
@@ -6,8 +6,30 @@ import ResturantCard from './ResturantCard';
 const Resturant = () => {
 
     const [menuDatas, setMenuData] = useState(Menu)
+
+    const filterItem = (category)=>{
+        const upDateValue = Menu.filter((curElem) => {
+            return curElem.category === category
+        } )
+
+        setMenuData(upDateValue);
+    }
+
+
+   
     return (
         <>
+        <nav className='navbar'>
+            <div className='btn-group'>
+                <button onClick={()=>filterItem("breakfast")}  className='btn-group__item'>Breakfast</button>
+                <button className='btn-group__item'>Lunch</button>
+                <button className='btn-group__item'>Evening</button>
+                <button className='btn-group__item'>Dinner</button>
+                <button className='btn-group__item'>All</button>
+
+            </div>
+
+        </nav>
               <div className='main-card--cointainer'>
               {
                     menuDatas.map(menu=> <ResturantCard
