dd248b9ed66d117435b3770814d08f4081660cc0
Mehadyhasanakash <mehadyhasanakash@gmail.com>
2022-04-08 12:11:19 +0600

    second-commmit

--- src/Component/Basic/Resturant.js
+++ src/Component/Basic/Resturant.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./style.css"
+import Menu  from "../MenuApi.json/MenuApi"
 
 const Resturant = () => {
     return (
@@ -8,8 +9,19 @@ const Resturant = () => {
                <div className='card'>
                    <div className='card-body'>
                        <span className='card-number card-circle subtle'>1</span>
+                       <span className='card-author'> Breakfast</span>
+                       
+                       <h2 className='card-title'> Maggi</h2>
 
+                       <span className='card-description subtle'>
+
+                           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum temporibus odit accusamus fugit eligendi animi aspernatur, consectetur omnis hic eveniet expedita nobis molestias delectus quia commodi? Perferendis quam velit nam dolores provident ipsa, accusantium, excepturi aliquam eius officia perspiciatis tempore.
+                       </span>
+                        <div className='card-read'>Read</div>
                    </div>
+                   <img src={image} alt="image" className='card-media' />
+
+                   <span className='card-tag'> oder now</span>
 
                </div>
 
